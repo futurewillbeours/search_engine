12e057b1087e81305ce63657f763c1f2a0365764
Ilya <zakananis@ya.ru>
2023-09-30 08:17:18 +0300

    m

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+using namespace std;
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -5,6 +7,7 @@
 #include <ctime>
 #include <filesystem>
 #include <map>
+#include <cassert>
 #include "nlohmann/json.hpp"
 #include "gtest/gtest.h"
 
@@ -18,16 +21,15 @@
 #define MAX_WORDS_IN_REQUEST 10
 
 //project parameters
-#define PROJECT_NAME "SearchEngine"
+#define PROJECT_NAME "search_engine"
 #define PROJECT_VERSION "0.1"
 #define MAX_RESPONSES 6
 
-TEST(sample_test_case, sample_test)
-{
-    EXPECT_EQ(1, 1);
-}
-
 std::filesystem::path resourcesPath(RESOURCES_FOLDER);
+struct Entry {
+    size_t doc_id, count;
+    bool operator == (const Entry& other) const {return (doc_id == other.doc_id && count == other.count);}
+};
 
 class ConverterJSON {
     public:
@@ -85,11 +87,50 @@ class ConverterJSON {
 };
 
 class InvertedIndex {
-    struct Entry { size_t doc_id, count; };
     std::vector<std::string> docs;
     std::map<std::string, std::vector<Entry>> freq_dictionary;
 
+    public:
+    InvertedIndex() = default;
+
+    void UpdateDocumentBase (std::vector<std::string>input_docs) {}
+
+    std::vector<Entry> GetWordCount (const std::string& word);
 };
+
+//TESTS
+// void TestInvertedIndexFunctionality (const vector<string>& docs, const vector<string>& requests, const std::vector<vector<Entry>>& expected) {
+//     std::vector<std::vector<Entry>> result;
+//     InvertedIndex idx;
+//     idx.UpdateDocumentBase(docs);
+//     for(auto& request : requests) {
+//         std::vector<Entry> word_count = idx.GetWordCount(request);
+//         result.push_back(word_count);
+//     }
+//     ASSERT_EQ(result, expected);
+// }
+
+// TEST(TestCaseInvertedIndex, TestBasic) {
+//     const vector<string> docs = {"london is the capital of great britain", "big ben is the nickname for the Great bell of the striking clock"};
+//     const vector<string> requests = {"london", "the"};
+//     const vector<vector<Entry>> expected = { { {0, 1} }, { {0, 1}, {1, 3} } };
+//     TestInvertedIndexFunctionality(docs, requests, expected);
+// }
+
+// TEST(TestCaseInvertedIndex, TestBasic2) {
+//     const vector<string> docs = { "milk milk milk milk water water water", "milk water water", "milk milk milk milk milk water water water water water", "americano cappuccino"};
+//     const vector<string> requests = {"milk", "water", "cappuchino"};
+//     const vector<vector<Entry>> expected = { { {0, 4}, {1, 1}, {2, 5} }, { {0, 2}, {1, 2}, {2, 5} }, { {3, 1} } };
+//     TestInvertedIndexFunctionality(docs, requests, expected);
+// }
+
+// TEST(TestCaseInvertedIndex, TestInvertedIndexMissingWord) {
+//     const vector<string> docs = { "a b c d e f g h i j k l", "statement" };
+//     const vector<string> requests = {"m", "statement"};
+//     const vector<vector<Entry>> expected = { { }, { {1, 1} } };
+//     TestInvertedIndexFunctionality(docs, requests, expected);
+// }
+//TESTS
 
 class Generator {
     public:
