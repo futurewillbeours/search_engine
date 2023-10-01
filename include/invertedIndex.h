ffc1efa8e13d4415eaf619278926c4db122dd72f
Ilya <zakananis@ya.ru>
2023-10-01 16:46:18 +0300

    m

--- include/invertedIndex.h
+++ include/invertedIndex.h
@@ -13,11 +13,14 @@ class InvertedIndex {
     std::vector<std::string> docs;
     std::map<std::string, std::vector<Entry>> freq_dictionary;
     std::vector<std::thread> threads;
+    std::vector<std::unique_ptr<std::thread>> pointers;
+    std::mutex mutex;
 
     public:
     InvertedIndex() = default;
 
     void indexation(int i) {
+        mutex.lock();
         std::stringstream stringStream(docs[i]);
         do {
             std::string buffer;
@@ -44,17 +47,23 @@ class InvertedIndex {
                     entry.doc_id = i;
                     freq_dictionary[buffer].push_back(entry);
                 }
-            }
+            } 
         } while (stringStream);
+        mutex.unlock();
     }
 
     void UpdateDocumentBase (std::vector<std::string> input_docs) {
         docs = input_docs;
-        for(int i = 0; i < docs.size(); i++) threads.push_back(std::thread(&InvertedIndex::indexation, this, i));
-        for(int i = 0; i < docs.size(); i++) threads[i].join();
+        for(int i = 0; i < docs.size(); i++) {
+            //pointers.push_back(std::make_unique<std::thread>(std::thread(&InvertedIndex::indexation, this, i)));
+            threads.push_back(std::thread(&InvertedIndex::indexation, this, i));
+
+        } 
+        for(int i = 0; i < docs.size(); i++) {
+            //pointers[i].get()->join();
+            threads[i].join();
+        }
     }
 
-    std::vector<Entry> GetWordCount(const std::string& word) {
-        return freq_dictionary[word];
-    }
+    std::vector<Entry> GetWordCount(const std::string& word) {return freq_dictionary[word];}
 };
