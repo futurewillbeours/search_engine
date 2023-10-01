c48122d70847e5feeeeccef44e45f8267458c9e1
Ilya <zakananis@ya.ru>
2023-10-01 10:46:42 +0300

    m

--- include/invertedIndex.h
+++ include/invertedIndex.h
@@ -15,39 +15,42 @@ class InvertedIndex {
     public:
     InvertedIndex() = default;
 
-    void UpdateDocumentBase (std::vector<std::string> input_docs) {
-        docs = input_docs;
-        for(int i = 0; i < docs.size(); i++) {
-            std::stringstream stringStream(docs[i]);
-            do {
-                std::string buffer;
-                stringStream >> buffer;
-                if(freq_dictionary.count(buffer) == 0) {
+    void indexation(int i) {
+        std::stringstream stringStream(docs[i]);
+        do {
+            std::string buffer;
+            stringStream >> buffer;
+            if(freq_dictionary.count(buffer) == 0) {
+                std::vector<Entry> vec;
+                Entry entry;
+                entry.count = 1;
+                entry.doc_id = i;
+                vec.push_back(entry);
+                freq_dictionary[buffer] = vec;
+            } else {
+                bool isFound = false;
+                for(int j = 0; j < freq_dictionary[buffer].size(); j++) {
+                    if(i == freq_dictionary[buffer][j].doc_id) {
+                        isFound = true;
+                        freq_dictionary[buffer][j].count++;
+                    }
+                }
+                if (isFound == false) {
                     std::vector<Entry> vec;
                     Entry entry;
                     entry.count = 1;
                     entry.doc_id = i;
-                    vec.push_back(entry);
-                    freq_dictionary[buffer] = vec;
-                } else {
-                    bool isFound = false;
-                    for(int j = 0; j < freq_dictionary[buffer].size(); j++) {
-                        if(i == freq_dictionary[buffer][j].doc_id) {
-                            isFound = true;
-                            freq_dictionary[buffer][j].count++;
-                        }
-                    }
-                    if (isFound == false) {
-                        std::vector<Entry> vec;
-                        Entry entry;
-                        entry.count = 1;
-                        entry.doc_id = i;
-                        freq_dictionary[buffer].push_back(entry);
-                    }
+                    freq_dictionary[buffer].push_back(entry);
                 }
-            } while (stringStream);
-        }
+            }
+        } while (stringStream);
     }
 
+    void UpdateDocumentBase (std::vector<std::string> input_docs) {
+        docs = input_docs;
+        for(int i = 0; i < docs.size(); i++) {
+            indexation(i);
+        }
+    }
     std::vector<Entry> GetWordCount (const std::string& word) {return freq_dictionary[word];}
 };
