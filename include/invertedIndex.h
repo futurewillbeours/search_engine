14bddb2db72ab3242f413662843e166ff19e2cd8
Ilya <zakananis@ya.ru>
2023-10-01 15:08:52 +0300

    m

--- include/invertedIndex.h
+++ include/invertedIndex.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <thread>
+#include <memory>
 #include "generator.h"
 
 struct Entry {
@@ -11,6 +12,7 @@ struct Entry {
 class InvertedIndex {
     std::vector<std::string> docs;
     std::map<std::string, std::vector<Entry>> freq_dictionary;
+    std::vector<std::thread> threads;
 
     public:
     InvertedIndex() = default;
@@ -48,9 +50,11 @@ class InvertedIndex {
 
     void UpdateDocumentBase (std::vector<std::string> input_docs) {
         docs = input_docs;
-        for(int i = 0; i < docs.size(); i++) {
-            indexation(i);
-        }
+        for(int i = 0; i < docs.size(); i++) threads.push_back(std::thread(&InvertedIndex::indexation, this, i));
+        for(int i = 0; i < docs.size(); i++) threads[i].join();
     }
-    std::vector<Entry> GetWordCount (const std::string& word) {return freq_dictionary[word];}
+
+    std::vector<Entry> GetWordCount(const std::string& word) {
+        return freq_dictionary[word];
+    }
 };
--- include/searchServer.h
+++ include/searchServer.h
@@ -1,18 +1,18 @@
-#pragma once
-#include "generator.h"
-#include "invertedIndex.h"
+// #pragma once
+// #include "generator.h"
+// #include "invertedIndex.h"
 
-struct RelativeIndex {
-    size_t doc_id;
-    float rank;
-    bool operator == (const RelativeIndex& other) const {return (doc_id == other.doc_id && rank == other.rank);}
-};
+// struct RelativeIndex {
+//     size_t doc_id;
+//     float rank;
+//     bool operator == (const RelativeIndex& other) const {return (doc_id == other.doc_id && rank == other.rank);}
+// };
 
-class SearchServer {
-    public:
-    SearchServer(InvertedIndex& idx) : _index(idx) {};
+// class SearchServer {
+//     public:
+//     SearchServer(InvertedIndex& idx) : _index(idx) {};
 
-    std::vector<std::vector<RelativeIndex>> search(const std::vector<std::string>& queries_input);
-    private:
-    InvertedIndex _index;
-};
+//     std::vector<std::vector<RelativeIndex>> search(const std::vector<std::string>& queries_input);
+//     private:
+//     InvertedIndex _index;
+// };
