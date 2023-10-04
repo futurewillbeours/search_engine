503e7103c6eb960982f80fcd3c1a5da77bd8b76b
Ilya <zakananis@ya.ru>
2023-10-04 11:24:28 +0300

    m

--- include/searchServer.h
+++ include/searchServer.h
@@ -15,7 +15,19 @@ class SearchServer {
 
     SearchServer(InvertedIndex& idx) : index(idx) {}
 
-    std::vector<std::vector<RelativeIndex>> Search(const std::vector<std::string>& queries_input) {
+    std::vector<std::vector<RelativeIndex>> Search(const std::vector<std::string>& queriesInput) {
+        std::vector<std::string> uniqueWords;
+        for (int i = 0; i < queriesInput.size(); i++) {
+            bool isFound = false;
+            for (int j = 0; j < uniqueWords.size(); j++) {
+                if (queriesInput[i] == uniqueWords[j]) {
+                    isFound = true;
+                    break;
+                }
+            }
+            if(isFound == false) uniqueWords.push_back(queriesInput[i]);
+        }
+
         
     }
 };
