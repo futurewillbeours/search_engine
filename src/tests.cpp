2445f84b922f531b1c06fae71f9e6a5bb0d4a6b7
Ilya <zakananis@ya.ru>
2023-11-06 09:28:56 +0300

    m

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,10 +1,6 @@
 #include "nlohmann/json.hpp"
 #include "gtest/gtest.h"
 
-// #include "./../include/converterJSON.h"
-// #include "./../include/invertedIndex.h"
-// #include "./../include/searchServer.h"
-// #include "./../include/additional.h"
 #include "./../include/tests.h"
 
 int main (int argc, char* argv[]) {
