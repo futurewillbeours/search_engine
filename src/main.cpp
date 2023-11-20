241a7e097d29d4cba815696e7f1004f47f83e347
Ilya <zakananis@ya.ru>
2023-11-20 19:57:04 +0300

    m

--- src/main.cpp
+++ src/main.cpp
@@ -17,5 +17,11 @@ int main(int argc, char* argv[]) {
         auto searchServer = new SearchServer(*idx);
         converter -> putAnswers(transform(searchServer->search(converter->getRequests())));
         std::cout << "Program finished, answers.json file created!\n";
-    } catch (const std::runtime_error& x) {std::cerr << "Caught runtime_error exception: " << x.what() << std::endl;}
+    } catch (const std::runtime_error& x) {
+        std::cerr << "Caught runtime_error exception: " << x.what() << std::endl;
+        std::cout << "Program finished with runtime error!\n";
+    } catch (const nlohmann::json_abi_v3_11_2::detail::parse_error& x) {
+        std::cerr << "Caught nlohmann_json exception: " << x.what() << std::endl;
+        std::cout << "Program finished with nlohmann_json file error!\n";
+    }
 }
